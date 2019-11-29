4f276ef5dfb4e67a345a50d3953247b38ca0c1d7
Martin Gaston <35241716+martingaston@users.noreply.github.com>
2019-11-29 18:49:11 +0000

    Functional test now checks we can input the first to-do list item

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -14,18 +16,30 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retreive_it_later(self):
         # Edith has heard of a cool new online todo application. She visits the homepage.
         self.browser.get('http://localhost:8000')
+        header_text = self.browser.find_element_by_tag_name('h1').text
 
-        # She can see the page title mentions to-do lists
+        # She can see the page title and header mentions to-do lists
         self.assertIn('To-Do', self.browser.title)
-        self.fail("Finish the test!")
+        self.assertIn('To-Do', header_text)
 
         # She can enter a to-do item straight away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
 
         # She types 'buy peacock feathers' into a text box
+        inputbox.send_keys("Buy peacock feathers")
 
         # When she hits 'Enter' the page updates and now '#1: Buy peacock feathers' appears as an item in the to-do list
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+
+        self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows))
 
         # There is still a text box where she can enter another item. She inputs 'use peacock feathers to make a fly'
+        self.fail("Finish the test!")
 
         # The page updates again and both items are in the list
 
