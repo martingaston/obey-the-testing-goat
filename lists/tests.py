4cc60292b5831b6bcca82184032d2aa27f51a533
martingaston <35241716+martingaston@users.noreply.github.com>
2020-01-04 00:38:22 +0000

    move URL in templates, add new URL for adding a new list and add/update views

--- lists/tests.py
+++ lists/tests.py
@@ -28,22 +28,17 @@ class HomePageTest(TestCase):
         response = self.client.get("/")
         self.assertTemplateUsed(response, "home.html")
 
-    def test_only_saves_when_necessary(self):
-        self.client.get('/')
-        self.assertEqual(Item.objects.count(), 0)
-
+class NewListTest(TestCase):
     def test_can_save_post_request(self):
-        response = self.client.post("/", data={"item_text":"A new list item"})
+        response = self.client.post("/lists/new", data={"item_text":"A new list item"})
 
         self.assertEqual(Item.objects.count(), 1)
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, "A new list item")
 
     def test_redirects_after_post(self):
-        response = self.client.post("/", data={"item_text":"A new list item"})
-
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
+        response = self.client.post("/lists/new", data={"item_text":"A new list item"})
+        self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
 
 class ListViewTest(TestCase):
     def test_uses_list_template(self):
