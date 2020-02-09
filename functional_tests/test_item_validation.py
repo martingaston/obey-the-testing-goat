2af7888645f8efaed7b09a5aa0893741bb7842e2
Martin Gaston <35241716+martingaston@users.noreply.github.com>
2020-02-09 18:36:54 +0000

    add test details to functional test test_item_validation.py

--- functional_tests/test_item_validation.py
+++ functional_tests/test_item_validation.py
@@ -4,15 +4,31 @@ from selenium.webdriver.common.keys import Keys
 class ItemValidationTest(FunctionalTest):
     def test_cannot_add_empty_list_items(self):
         # Edith returns to the home page and acidentally hits enter on an empty list input
+        self.browser.get(self.live_server_url)
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
 
         # The home page refreshes and an error is now displayed saying items cannot be blank
+        self.wait_for(lambda: self.assertEqual(
+            self.browser.find_element_by_css_selector('.has-error').text,
+            "You can't have an empty list item"
+        ))
 
-        # She tried again with text for the item, which works
+        # She tries again with text for the item, which works
+        self.browser.find_element_by_id('id_new_item').send_keys('Buy milk')
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy milk')
 
         # She now tries to submit a second blank list item
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
 
         # A similar warning as before is displayed on the list page
+        self.wait_for(lambda: self.assertEqual(
+            self.browser.find_element_by_css_selector('.has-error').text,
+            "You can't have an empty list item"
+        ))
 
         # And she corrects the error by submitting a todo with text
-
-        self.fail('write the test!')
+        self.browser.find_element_by_id('id_new_item').send_keys('Make tea')
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy milk')
+        self.wait_for_row_in_list_table('2: Make tea')
